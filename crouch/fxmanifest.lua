718c1372c0d143fc0bc61b336943bd09c997482a
EddieK96 <69137506+EddieK96@users.noreply.github.com>
2020-10-24 20:16:37 +0200

    Add files via upload

---
+++ crouch/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Crouch and stealth aim script. Made by Eddie; Feel free to share with right credit.'
+
+version '0.8'
+
+client_scripts {
+	'client/*.lua',
+	'config.lua'
+}
