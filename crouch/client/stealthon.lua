6528567671c2d436c1654b4e1038a0e8a26130d1
EddieK96 <69137506+EddieK96@users.noreply.github.com>
2021-04-28 13:50:26 +0200

    Update stealthon.lua

--- crouch/client/stealthon.lua
+++ crouch/client/stealthon.lua
@@ -4,19 +4,33 @@ Citizen.CreateThread(function()
 	stealth.forced = false
 	stealth.initialized = true
 	local playerped = PlayerPedId()
+	local player = GetPlayerIndex()
+	local function notify(text)
+		SetTextComponentFormat('STRING')
+		AddTextComponentString(text)
+		DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+	end
 	while config == nil or config.initialized == nil or cor == nil or cor.initialized == nil do
 		Citizen.Wait(0)
 	end
 	
 	while true do
 		Citizen.Wait(0)
-		
+		playerped = PlayerPedId()
+		player = GetPlayerIndex()
 		if stealth.forced then
-			SetPedStealthMovement(playerped, stealth.on, "DEFAULT_ACTION")
+		--	if stealth.on then
+				--notify("stealth")
+			--	TaskForceMotionState()
+		--end
+			SetPedStealthMovement(playerped, stealth.on, 0)	
 		end	
 		
 		if cor.forcedActionmode then
-			SetPedUsingActionMode(playerped, cor.actionmode, -1, "DEFAULT_ACTION")
+			SetPedUsingActionMode(playerped, cor.actionmode, 0, "DEFAULT_ACTION")
+			if not cor.actionmode then
+				RemoveActionModeAsset(-1)
+			end
 		end	
 	end
 end)
